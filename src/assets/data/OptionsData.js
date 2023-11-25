321e753da0862dc4015cb156b8ea204ab4f3da17
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-11-25 07:50:06 +0100

    changes styles - options

--- src/assets/data/OptionsData.js
+++ src/assets/data/OptionsData.js
@@ -1,21 +1,26 @@
+import star from "../../assets/images/star-2.svg";
+
 const OptionsData = [
   {
     id: 1,
-    icon: "",
-    title: "",
-    description: "",
+    icon: star,
+    title: "Casual breakfast",
+    description:
+      "Start your day with a hearty and satisfying breakfast in a relaxed setting. Its all about comfort and convenience, offering a variety of options to kick-start your morning.",
   },
   {
-    id: 1,
-    icon: "",
-    title: "",
-    description: "",
+    id: 2,
+    icon: star,
+    title: "Bar & Lounge",
+    description:
+      "For a relaxed vibe, our bar and lounge offer drinks, snacks, and a friendly atmosphere, perfect for catching up with friends or sipping on cocktails.",
   },
   {
-    id: 1,
-    icon: "",
-    title: "",
-    description: "",
+    id: 3,
+    icon: star,
+    title: "Elegant dinning",
+    description:
+      "For those special occasions or romantic evenings, our elegant dining category provides a sophisticated and intimate atmosphere.",
   },
 ];
 
--- src/components/options/index.jsx
+++ src/components/options/index.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import { OptionsCard } from "../../components";
 import OptionsData from "../../assets/data/OptionsData";
+import "../../styles/Options.scss";
+import optionimg from "../../assets/images/optionsimg.png";
 
 const Options = () => {
   return (
     <div className="options_wrapper">
       <header>
-        <h1>dining and resturant options</h1>
-        <img src="" alt="optionsbg" />
+        <h1 className="behindheader">dining and resturant options</h1>
+        <div>
+          <img src={optionimg} alt="optionsbg" />
+          <h1 className="frontheader">dining and resturant options</h1>
+        </div>
       </header>
 
       <div>
-        {OptionsData.forEach((option) => {
+        {OptionsData.map((option) => (
           <OptionsCard
-            id={option.id}
+            key={option.id}
             icon={option.icon}
             title={option.title}
             description={option.description}
-          />;
-        })}
+          />
+        ))}
       </div>
     </div>
   );
